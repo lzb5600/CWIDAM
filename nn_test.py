9fd3832156b2e9798ba9fc54a8e9f29afd0fa033
CWID <50892161+CWIDAM@users.noreply.github.com>
2019-05-24 21:01:46 +1000

    Update nn_test.py

--- nn_test.py
+++ nn_test.py
@@ -7,36 +7,7 @@ from torch import optim, nn
 from torch.utils.data.dataset import Dataset
 from sklearn.preprocessing import StandardScaler
 
-class send_para(object):
-    def __init__(self,data_dir='./data/sensor/',ld=16,lr=0.01,momentum=0.5,batch_size=16,epochs=100,nh=100,missingrate=0.1,test_seed=0,get_train=True):
-        self.ld = ld
-        self.lr = lr
-        self.momentum = momentum
-        self.batch_size = batch_size
-        self.epochs = epochs
-        self.nh = nh
-        
-        if get_train:
-            
-            self.trset = np.load(data_dir + 'trset.npy')
-            np.random.seed(6)
-            self.trset[np.random.choice([False,True],size=self.trset.shape,p=[1-missingrate,missingrate])]=np.nan
-            
-            self.vaset = np.load(data_dir + 'vaset.npy')
-            np.random.seed(7)
-            self.vaset[np.random.choice([False,True],size=self.vaset.shape,p=[1-missingrate,missingrate])]=np.nan
-            
-            self.trlabels = np.load(data_dir + 'trlabels.npy')
-            self.valabels = np.load(data_dir + 'valabels.npy')
-            
-        else:
-            
-            self.teset = np.load(data_dir + 'teset.npy')
-            np.random.seed(test_seed)
-            self.teset[np.random.choice([False,True],size=self.teset.shape,p=[1-missingrate,missingrate])]=np.nan
-            self.telabels = np.load(data_dir + 'telabels.npy')
-        
-        
+       
         
         
 class LoadData(Dataset):
@@ -83,7 +54,7 @@ def test(args, model, device, test_loader, criterion):
 
 
 class ScaleNet(nn.Module):
-    def __init__(self, fd, n_class, ld, num_hidden):
+    def __init__(self, fd, n_class, ld, num_hidden=100):
         super().__init__()
         self.weight1 = nn.Parameter(torch.empty(fd*2,ld))  # define the trainable parameter fd:feature dimension
         self.weight1.data.uniform_(-0.1, 0.1)              # initialise
@@ -102,63 +73,51 @@ class ScaleNet(nn.Module):
         return x
 
 
-if __name__ == '__main__':   
+if __name__ == '__main__':
+    
     data_dir = './data/sensor/'
-    p = np.load("./best_para/scalenet_sensor_best.npy")
-    results_mean = []
-    results_std = []
-    for i,missing_rate in enumerate([0.1,0.3,0.5,0.7,0.9]):
-                    
-        args = send_para(data_dir=data_dir,ld=p[i],missingrate=missing_rate,test_seed=i)
-        trset = args.trset
-        trlabels = args.trlabels
-        vaset = args.vaset   
-        valabels = args.valabels
-        
-        scaler = StandardScaler()
-        trset = scaler.fit_transform(trset)
-        vaset = scaler.transform(vaset)
-        
-        device = torch.device("cuda")
-        
-        train_loader = torch.utils.data.DataLoader(
-                       dataset=LoadData(trset,trlabels),
-                       batch_size=args.batch_size, 
-                       shuffle=True)
-        va_loader = torch.utils.data.DataLoader(
-                      dataset=LoadData(vaset,valabels),
-                      batch_size=1024, 
-                      shuffle=False)
+    trset = np.load(data_dir + 'trset.npy')
+    teset = np.load(data_dir + 'teset.npy')
+    vaset = np.load(data_dir + 'vaset.npy')
+    trlabels = np.load(data_dir + 'trlabels.npy')
+    telabels = np.load(data_dir + 'telabels.npy')
+    valabels = np.load(data_dir + 'telabels.npy')
+ 
+    k = 10 # rank(H)<=k
+    scaler = StandardScaler()
+    trset = scaler.fit_transform(trset)
+    teset = scaler.transform(teset)
+    vaset = scaler.transform(vaset)
 
-        test_acc = []
-        for rep in range(5):
-        
-            best_acc_ld = 0
-            
-            model = ScaleNet(fd=trset.shape[1], n_class=int(trlabels.max()+1), ld=args.ld, num_hidden=args.nh).to(device)
-            criterion = nn.CrossEntropyLoss()
-            optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-        
+    device = torch.device("cuda")
 
-            for epoch in range(1, args.epochs + 1):
-                train(args, model, device, train_loader, criterion, optimizer, epoch)
-                va_accuracy = test(args, model, device, va_loader, criterion)
-                if va_accuracy >= best_acc_ld:
-                    best_acc_ld = va_accuracy
-                    best_model = copy.deepcopy(model)
+    train_loader = torch.utils.data.DataLoader(
+                   dataset=LoadData(trset,trlabels),
+                   batch_size=args.batch_size, 
+                   shuffle=True)
+    va_loader = torch.utils.data.DataLoader(
+                   dataset=LoadData(vaset,valabels),
+                   batch_size=1024, 
+                   shuffle=False)
+    te_loader = torch.utils.data.DataLoader(
+                   dataset=LoadData(teset,telabels),
+                   batch_size=1024, 
+                   shuffle=False)
 
-            args = send_para(data_dir=data_dir,ld=p[i],missingrate=missing_rate,test_seed=rep,get_train=False)
-            teset = args.teset   
-            telabels = args.telabels
-            teset = scaler.transform(teset)
-            te_loader = torch.utils.data.DataLoader(
-                        dataset=LoadData(teset,telabels),
-                        batch_size=1024, 
-                        shuffle=False)
-            test_acc.append(test(args, best_model, device, te_loader, criterion))          
-        print(np.mean(test_acc),np.std(test_acc))
-        results_mean.append(np.mean(test_acc))
-        results_std.append(np.std(test_acc))
-    np.save("./results/scalenet_sensor_results", np.vstack((np.array(results_mean),np.array(results_std))))
-                     
+
+    model = ScaleNet(fd=trset.shape[1], n_class=int(trlabels.max()+1), ld=k, num_hidden=100).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+    best_acc = 0
+    for epoch in range(1, 200):
+        train(args, model, device, train_loader, criterion, optimizer, epoch)
+        va_accuracy = test(args, model, device, va_loader, criterion)
+        if va_accuracy >= best_acc:
+            best_acc = va_accuracy
+            best_model = copy.deepcopy(model)
+    
+    test_acc=test(args, best_model, device, te_loader, criterion)
+    print(test_acc)
+
 
